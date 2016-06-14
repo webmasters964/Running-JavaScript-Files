1536a5a5dbed12626b9d937e8233b013250fe560
Sanjay Kumar <webmasters964@gmail.com>
2016-06-14 23:07:27 +0530

    Running JavaScript Files
    Running JavaScript Files in node.js

---
+++ app/Server.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Jay on 6/14/2016.
+ */
+console.log("Hello World! Running js Files in node.js ")
+
+console.log("------------------ Arithmetic operators ------------------")
+
+var x = 1+2;
+console.log("Addition :" + x)
+
+x = 1-2;
+console.log("Subtraction :" + x)
+
+x = 1*2;
+console.log("Multiplication :" + x)
+
+x = 1/2;
+console.log("Division :" + x)
+
+x = 1%2;
+console.log("Modulus (division remainder) :" + x)
+
